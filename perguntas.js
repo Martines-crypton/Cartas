644582f6973afda94f06313b443574ec608c504b
Martinsflix <00001103207489sp@al.educacao.sp.gov.br>
2024-11-22 18:28:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Aleatório',
+    'Quem tem 17 anos em 2024?',
+    'Eu,              otário          kkkkkkkkkkkkkkk'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'Aleatório',
+    'O que o mar falou a um náufrago que estava se afogando?',
+    'Nada'
+)
+
+criaCartao(
+    'Charada',
+    'Tenho rabo, mas não sou cão, não tenho asas, mas sei voar, se me largarem, não subo, mas saio ao vento a brincar, quem sou eu?',
+    'Uma pipa kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'
+)
